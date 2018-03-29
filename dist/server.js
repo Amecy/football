b1c13873517c8b419a8feef1218a74efebc1ab06
Amecy <920281921@qq.com>
2018-03-29 17:17:26 +0800

    修复：build 的资源路径

--- dist/server.js
+++ dist/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const proxy = require('http-proxy-middleware');
 
 const app = express();
-// app.use('/static', express.static(`${__dirname}/static`));
+app.use('/static', express.static(`${__dirname}/static`));
 // https://m.dongqiudi.com/home
 app.use('/api', proxy({
   target: 'https://api.dongqiudi.com',
@@ -12,7 +12,7 @@ app.use('/api', proxy({
   }
 }));
 
-// app.get('/*', (req, res) => {
-//   res.sendFile(`${__dirname}/index.html`);
-// });
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
 app.listen(8060);
